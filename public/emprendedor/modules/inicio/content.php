a95e589b5770d6eb4e248e8314efa8c4279aebd2
Juan Carlos HP <juan.gonzalez@alumnos.udg.mx>
2025-01-29 22:54:17 -0600

    La Linea Base Final desaparece para los Emprendedores

--- public/emprendedor/modules/inicio/content.php
+++ public/emprendedor/modules/inicio/content.php
@@ -1,41 +1,27 @@
 <div class="container my-5">
 
     <div class="row mb-4">
-        <div class="card shadow-sm border-top border-success rounded-3">
+        <div class="card shadow-sm border-top border-warning rounded-3">
             <div class="card-body">
                 <h5 class="card-title mb-3 fw-semibold">Accesos rápidos</h5>
-                <div class="d-flex justify-content-start align-items-center"> <!-- Ajuste de alineación en flexbox -->
-                    <!-- Dropdown -->
-                    <div class="dropdown me-2">
-                        <button class="btn btn-outline-success dropdown-toggle" type="button" id="dropdownLineaBase" data-bs-toggle="dropdown" aria-expanded="false">
-                            Línea Base
-                        </button>
-                        <ul class="dropdown-menu" aria-labelledby="dropdownLineaBase">
-                            <li>
-                                <a href="../lineaBase/" class="dropdown-item d-flex align-items-center">
-                                    <i class="ti ti-start me-2"></i> Inicial
-                                </a>
-                            </li>
-                            <li>
-                                <a href="../lineaFinal" class="dropdown-item d-flex align-items-center">
-                                    <i class="ti ti-end me-2"></i> Final
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <!-- Enlace "Mis talleres" -->
-                    <a href="#" class="btn btn-outline-success m-2 d-flex align-items-center justify-content-center">
+                <div class="d-flex justify-content-start align-items-center">
+                    <a href="../lineaBase/" class="btn btn-outline-primary m-2 d-flex align-items-center">
+                        <iconify-icon icon="streamline:navigation-arrow-on" class="fs-6 me-2"></iconify-icon>
+                        Línea Base
+                    </a>
+                    <a href="#" class="btn btn-outline-warning m-2 d-flex align-items-center">
+                        <iconify-icon icon="solar:square-academic-cap-line-duotone" class="fs-6 me-2"></iconify-icon>
                         Mis Talleres
                     </a>
                 </div>
+
             </div>
         </div>
     </div>
 
 
     <!-- Sección de Publicaciones -->
-    <div class="mb-4 p-4 border rounded-3 shadow-sm bg-light">
+    <div class="mb-4 p-4 border rounded-3 shadow-sm bg-body-secondary">
         <div class="d-flex justify-content-between align-items-center mb-3">
             <h2 class="h4 mb-0">Publicaciones</h2>
             <div>
