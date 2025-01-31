fe7edd3af4c655fba1589cd8f0c53d603f2ad0e4
Juan Carlos HP <juan.gonzalez@alumnos.udg.mx>
2025-01-30 22:50:06 -0600

    Funciones de Linea Base borradas

--- public/administradorProyectos/modules/seguimientoCaso/api/seguimientoCaso.js
+++ public/administradorProyectos/modules/seguimientoCaso/api/seguimientoCaso.js
@@ -23,10 +23,9 @@ function ready() {
 
 function construirTablaEmprendedores(data) {
     const dataTabla = data.emprendedores.map(emprendedor => ({
-            "Etapa": generarListaEtapas(emprendedor.idEtapa, data.etapas, emprendedor.idLineaBase),
+            "Etapa": emprendedor.etapa,
             "Nombre": `${emprendedor.nombre} ${emprendedor.apellidos}`,
             "Correo electrónico": emprendedor.correo,
-            "Linea base": crearBotonesLineaBase(emprendedor),
             "Seguimiento de caso": crearBotonSeguimiento(emprendedor)
         }));
     if (dataTabla.length !== 0) {
@@ -46,131 +45,6 @@ function construirSelectorEtapas(etapas) {
     $selector.val(etapas.estapaSeleccionada);
 }
 
-function crearBotonesLineaBase(emprendedor) {
-    return $('<div>', {
-        class: 'btn-group',
-        role: 'group',
-        'aria-label': 'Botones Linea Base'
-    }).append(
-            // Botón para "Inicial"
-            $('<div>', {class: 'btn-group'}).append(
-            $('<button>', {
-                type: 'button',
-                class: 'btn btn-sm btn-outline-success dropdown-toggle',
-                'data-bs-toggle': 'dropdown',
-                'aria-expanded': 'false',
-                text: 'Inicial'
-            }),
-            $('<ul>', {class: 'dropdown-menu'}).append(
-            $('<li>').append(
-            $('<a>', {
-                class: 'dropdown-item',
-                href: '#',
-                click: function (e) {
-                    e.preventDefault();
-                    lineaBaseAction('inicial', 'Ver', emprendedor.idUsuario);
-                }
-            }).append(
-            $('<i>', {class: 'ti ti-file-search me-2', title: 'Ver'}),
-            'Ver'
-            )
-            ),
-            $('<li>').append(
-            $('<a>', {
-                class: 'dropdown-item',
-                href: '#',
-                click: function (e) {
-                    e.preventDefault();
-                    lineaBaseAction('inicial', 'Modificar', emprendedor.idUsuario);
-                }
-            }).append(
-            $('<i>', {class: 'ti ti-edit me-2', title: 'Modificar'}),
-            'Modificar'
-            )
-            ),
-            $('<li>').append(
-            $('<a>', {
-                class: 'dropdown-item',
-                href: '#',
-                click: function (e) {
-                    e.preventDefault();
-                    eliminarlineaBase('inicial', emprendedor.idUsuario);
-                }
-            }).append(
-            $('<i>', {class: 'ti ti-trash me-2', title: 'Eliminar'}),
-            'Eliminar'
-            )
-            )
-            )
-            ),
-            // Botón para "Final"
-            $('<div>', {class: 'btn-group'}).append(
-            $('<button>', {
-                type: 'button',
-                class: 'btn btn-sm btn-outline-success dropdown-toggle',
-                'data-bs-toggle': 'dropdown',
-                'aria-expanded': 'false',
-                text: 'Final'
-            }),
-            $('<ul>', {class: 'dropdown-menu'}).append(
-            $('<li>').append(
-            $('<a>', {
-                class: 'dropdown-item',
-                href: '#',
-                click: function (e) {
-                    e.preventDefault();
-                    lineaBaseAction('final', 'Ver', emprendedor.idUsuario);
-                }
-            }).append(
-            $('<i>', {class: 'ti ti-file-search me-2', title: 'Ver'}),
-            'Ver'
-            )
-            ),
-            $('<li>').append(
-            $('<a>', {
-                class: 'dropdown-item',
-                href: '#',
-                click: function (e) {
-                    e.preventDefault();
-                    lineaBaseAction('final', 'Modificar', emprendedor.idUsuario);
-                }
-            }).append(
-            $('<i>', {class: 'ti ti-edit me-2', title: 'Modificar'}),
-            'Modificar'
-            )
-            ),
-            $('<li>').append(
-            $('<a>', {
-                class: 'dropdown-item',
-                href: '#',
-                click: function (e) {
-                    e.preventDefault();
-                    eliminarlineaBase('final', emprendedor.idUsuario);
-                }
-            }).append(
-            $('<i>', {class: 'ti ti-trash me-2', title: 'Eliminar'}),
-            'Eliminar'
-            )
-            )
-            )
-            )
-            );
-}
-
-
-function lineaBaseAction(tipo, action, id) {
-    const data = {
-        idUsuario: id,
-        tipo: tipo
-    };
-    crearPeticion(urlAPI, {case: 'lineaBaseAction', data: $.param(data)}, (rs) => {
-        if (rs.success) {
-            redireccionar("../lineaBase" + action);
-        } else {
-            mostrarMensajeError("Intenta más tarde: " + rs.msg);
-        }
-    });
-}
 
 function crearBotonSeguimiento(emprendedor) {
     var $boton = $('<button>', {
@@ -213,45 +87,5 @@ function eliminarSeguimientoCaso(id) {
         mensajeAlerta: "Se eliminará el seguimiento de caso",
         url: urlAPI,
         data: {"case": "eliminarSeguimientoCaso", "data": `id=${id}`}
-    });
-}
-
-function generarListaEtapas(etapa, listaEtapas, idLineaBase) {
-    const $selector = $('<select>', {
-        class: 'form-select form-select-sm',
-        name: 'etapas',
-        'data-id-linea-base': idLineaBase
-    });
-    listaEtapas.forEach(val => {
-        const $option = $('<option>', {
-            value: val.idEtapa,
-            text: val.nombre
-        });
-        if (val.idEtapa === etapa)
-            $option.prop('selected', true);
-        $selector.append($option);
-    });
-
-    $selector.change(function () {
-        const data = $.param({
-            etapa: $(this).val(),
-            lineaBase: $(this).data("id-linea-base")
-        });
-        crearPeticion(urlAPI, {case: "actualizarEtapa", data: data}, (res) => {
-            if (res.es_valor_error === false) {
-                refresh();
-            }
-        });
-    });
-
-    return $selector;
-}
-
-
-function eliminarlineaBase(tipo, idUsuario) {
-    alertaEliminar({
-        mensajeAlerta: "La información de la linea base " + tipo + " ya no estará disponible",
-        url: urlAPI,
-        data: {case: "eliminarLineaBase", data: $.param({tipo: tipo, usuario: idUsuario})}
     });
 }
