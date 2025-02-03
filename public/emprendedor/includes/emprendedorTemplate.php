8097a6f3417d4cb74ef61a9fd3fa36efc9bbe0fb
Juan Carlos HP <juan.gonzalez@alumnos.udg.mx>
2025-02-02 22:01:30 -0600

    Ajuste para incluir controlarAccesoUbicacion()

--- public/emprendedor/includes/emprendedorTemplate.php
+++ public/emprendedor/includes/emprendedorTemplate.php
@@ -1,9 +1,12 @@
 
 <?php
 
+include '../../../../loader.php';
 include '../../../assets/commons/template.php';
 
 function renderizarPlantillaEmprendedor($dir, $scripts = []) {
     $aside = "../../includes/aside.php";
     inicializarPlantilla($dir, $aside, $scripts);
 }
+
+Sesion::controlarAccesoUbicacion("emprendedor");
