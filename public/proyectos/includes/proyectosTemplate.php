da56d57c33d01d4a2148af0b2f41f897c5fd65c9
Juan Carlos HP <juan.gonzalez@alumnos.udg.mx>
2025-02-02 22:01:35 -0600

    Ajuste para incluir controlarAccesoUbicacion()

--- public/proyectos/includes/proyectosTemplate.php
+++ public/proyectos/includes/proyectosTemplate.php
@@ -1,9 +1,12 @@
 
 <?php
 
+include '../../../../loader.php';
 include '../../../assets/commons/template.php';
 
 function renderizarPlantillaProyectos($dir, $scripts = []) {
     $aside = "../../includes/aside.php";
     inicializarPlantilla($dir, $aside, $scripts);
 }
+
+Sesion::controlarAccesoUbicacion("proyectos");
