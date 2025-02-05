ba65416b6fc4f1c76b03775736a9435ca4fdee9c
Juan Carlos HP <juan.gonzalez@alumnos.udg.mx>
2025-02-04 22:17:19 -0600

    Emprendedores sin llinea base

--- public/proyectos/modules/lineaBaseAdministracion/api/lineaBaseAdministracion.js
+++ public/proyectos/modules/lineaBaseAdministracion/api/lineaBaseAdministracion.js
@@ -4,6 +4,7 @@ function ready() {
     crearPeticion(urlAPI, {case: "recuperarEmprendedores"}, function (data) {
         construirSelectorEtapas(data.etapas);
         construirTablaEmprendedores(data);
+        construirTablaEmprendedoresSinLineaBase(data.emprendedoresNoLineaBase);
     });
 
     $("#filterForm").submit(function (e) {
@@ -21,23 +22,53 @@ function ready() {
     });
 }
 
-function construirTablaEmprendedores(data) {
-    const dataTabla = data.emprendedores.map(emprendedor => ({
-            "Etapa": generarListaEtapas(emprendedor.idEtapa, data.etapas, emprendedor.idLineaBase),
-            "Nombre": `<a href="#">${emprendedor.nombre} ${emprendedor.apellidos}</a>`,
-            //"Correo electrónico": emprendedor.correo,
-            "Linea base": crearBotonesLineaBase(emprendedor),
-        }));
+// Función genérica para construir tablas
+function construirTablaGenerica(data, columnas, contenedor, idTabla) {
+    const dataTabla = data.map(emprendedor => {
+        let fila = {};
+        columnas.forEach(columna => {
+            fila[columna.nombre] = columna.generar(emprendedor);
+        });
+        return fila;
+    });
+
     if (dataTabla.length !== 0) {
-        construirTablaDataTable(dataTabla, "table-hover table-bordered", "tablaEmprendedoresContenedor", "tablaEmprendedores", "");
+        // Si hay datos, construimos la tabla con DataTable
+        construirTablaDataTable(dataTabla, "table-hover table-bordered", contenedor, idTabla, "");
     } else {
-        const $tablaVacia = $('<table>', {class: "table table-bordered align-middle text-nowrap mb-1", id: "tablaEmprendedores"});
-        const $thead = $('<thead>').append($('<tr>').append($('<th>', {text: "Etapa"})).append($('<th>', {text: "Nombre"})).append($('<th>', {text: "Correo electrónico"})));
+        // Si no hay datos, mostramos una tabla vacía
+        const $tablaVacia = $('<table>', {class: "table table-bordered align-middle text-nowrap mb-1", id: idTabla});
+        const $thead = $('<thead>').append($('<tr>'));
+        columnas.forEach(columna => {
+            $thead.append($('<th>', {text: columna.titulo}));
+        });
         $tablaVacia.append($thead);
-        $("#tablaEmprendedoresContenedor").empty().append($tablaVacia);
-        crearDataTable("#tablaEmprendedores");
+        $("#" + contenedor).empty().append($tablaVacia);
+        crearDataTable("#" + idTabla);
     }
 }
+
+// Función específica para los emprendedores con línea base
+function construirTablaEmprendedores(data) {
+    const columnas = [
+        {nombre: "Etapa", titulo: "Etapa", generar: (emprendedor) => generarListaEtapas(emprendedor.idEtapa, data.etapas, emprendedor.idLineaBase)},
+        {nombre: "Nombre", titulo: "Nombre", generar: (emprendedor) => `<a href="#" class="text-info">${emprendedor.nombre} ${emprendedor.apellidos}</a>`},
+        {nombre: "Correo electrónico", titulo: "Correo electrónico", generar: (emprendedor) => emprendedor.correo},
+        {nombre: "Linea base", titulo: "Linea base", generar: (emprendedor) => crearBotonesLineaBase(emprendedor)}
+    ];
+    construirTablaGenerica(data.emprendedores, columnas, "tablaEmprendedoresContenedor", "tablaEmprendedores");
+}
+
+// Función específica para los emprendedores sin línea base
+function construirTablaEmprendedoresSinLineaBase(data) {
+    const columnas = [
+        {nombre: "Etapa", titulo: "Etapa", generar: (emprendedor) => emprendedor.etapa},
+        {nombre: "Nombre", titulo: "Nombre", generar: (emprendedor) => `<a href="#" class="text-info">${emprendedor.nombre} ${emprendedor.apellidos}</a>`},
+        {nombre: "Correo electrónico", titulo: "Correo electrónico", generar: (emprendedor) => emprendedor.correo}
+    ];
+    construirTablaGenerica(data, columnas, "tablaEmprendedoresSinLineaBaseContenedor", "tablaEmprendedoresSinLineaBase");
+}
+
 
 function construirSelectorEtapas(etapas) {
     const $selector = crearSelector(null, "etapa", etapas);
@@ -46,13 +77,7 @@ function construirSelectorEtapas(etapas) {
 }
 
 function crearBotonesLineaBase(emprendedor) {
-    return $('<div>', {
-        class: 'btn-group',
-        role: 'group',
-        'aria-label': 'Botones Linea Base'
-    }).append(
-            // Botón para "Inicial"
-            $('<div>', {class: 'btn-group'}).append(
+    return $('<div>', {class: 'btn-group'}).append(
             $('<button>', {
                 type: 'button',
                 class: 'btn btn-sm btn-outline-success dropdown-toggle',
@@ -61,99 +86,47 @@ function crearBotonesLineaBase(emprendedor) {
                 text: 'Inicial'
             }),
             $('<ul>', {class: 'dropdown-menu'}).append(
-            $('<li>').append(
-            $('<a>', {
-                class: 'dropdown-item',
-                href: '#',
-                click: function (e) {
-                    e.preventDefault();
-                    lineaBaseAction('inicial', 'Ver', emprendedor.idUsuario);
-                }
-            }).append(
-            $('<i>', {class: 'ti ti-file-search me-2', title: 'Ver'}),
-            'Ver'
+                $('<li>').append(
+                    $('<a>', {
+                        class: 'dropdown-item',
+                        href: '#',
+                        click: function (e) {
+                            e.preventDefault();
+                            lineaBaseAction('inicial', 'Ver', emprendedor.idUsuario);
+                        }
+                    }).append(
+                        $('<i>', {class: 'ti ti-file-search me-2', title: 'Ver'}),
+                        'Ver'
+                    )
+                ),
+                $('<li>').append(
+                    $('<a>', {
+                        class: 'dropdown-item',
+                        href: '#',
+                        click: function (e) {
+                            e.preventDefault();
+                            lineaBaseAction('inicial', 'Modificar', emprendedor.idUsuario);
+                        }
+                    }).append(
+                        $('<i>', {class: 'ti ti-edit me-2', title: 'Modificar'}),
+                        'Modificar'
+                    )
+                ),
+                $('<li>').append(
+                    $('<a>', {
+                        class: 'dropdown-item',
+                        href: '#',
+                        click: function (e) {
+                            e.preventDefault();
+                            eliminarlineaBase('inicial', emprendedor.idUsuario);
+                        }
+                    }).append(
+                        $('<i>', {class: 'ti ti-trash me-2', title: 'Eliminar'}),
+                        'Eliminar'
+                    )
+                )
             )
-            ),
-            $('<li>').append(
-            $('<a>', {
-                class: 'dropdown-item',
-                href: '#',
-                click: function (e) {
-                    e.preventDefault();
-                    lineaBaseAction('inicial', 'Modificar', emprendedor.idUsuario);
-                }
-            }).append(
-            $('<i>', {class: 'ti ti-edit me-2', title: 'Modificar'}),
-            'Modificar'
-            )
-            ),
-            $('<li>').append(
-            $('<a>', {
-                class: 'dropdown-item',
-                href: '#',
-                click: function (e) {
-                    e.preventDefault();
-                    eliminarlineaBase('inicial', emprendedor.idUsuario);
-                }
-            }).append(
-            $('<i>', {class: 'ti ti-trash me-2', title: 'Eliminar'}),
-            'Eliminar'
-            )
-            )
-            )
-            ),
-            // Botón para "Final"
-            $('<div>', {class: 'btn-group'}).append(
-            $('<button>', {
-                type: 'button',
-                class: 'btn btn-sm btn-outline-success dropdown-toggle',
-                'data-bs-toggle': 'dropdown',
-                'aria-expanded': 'false',
-                text: 'Seguimiento'
-            }),
-            $('<ul>', {class: 'dropdown-menu'}).append(
-            $('<li>').append(
-            $('<a>', {
-                class: 'dropdown-item',
-                href: '#',
-                click: function (e) {
-                    e.preventDefault();
-                    lineaBaseAction('final', 'Ver', emprendedor.idUsuario);
-                }
-            }).append(
-            $('<i>', {class: 'ti ti-file-search me-2', title: 'Ver'}),
-            'Ver'
-            )
-            ),
-            $('<li>').append(
-            $('<a>', {
-                class: 'dropdown-item',
-                href: '#',
-                click: function (e) {
-                    e.preventDefault();
-                    lineaBaseAction('final', 'Modificar', emprendedor.idUsuario);
-                }
-            }).append(
-            $('<i>', {class: 'ti ti-edit me-2', title: 'Modificar'}),
-            'Modificar'
-            )
-            ),
-            $('<li>').append(
-            $('<a>', {
-                class: 'dropdown-item',
-                href: '#',
-                click: function (e) {
-                    e.preventDefault();
-                    eliminarlineaBase('final', emprendedor.idUsuario);
-                }
-            }).append(
-            $('<i>', {class: 'ti ti-trash me-2', title: 'Eliminar'}),
-            'Eliminar'
-            )
-            )
-            )
-            )
-            );
+        );
 }
 
 
