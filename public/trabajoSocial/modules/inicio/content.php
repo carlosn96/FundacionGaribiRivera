2ac09b75959e9f9b83a2ea3b413f736101ce6f26
Juan Carlos HP <juan.gonzalez@alumnos.udg.mx>
2025-07-26 20:30:34 -0600

    Se cambia el dashboard de Trabajo Social

--- public/trabajoSocial/modules/inicio/content.php
+++ public/trabajoSocial/modules/inicio/content.php
@@ -1,62 +1,75 @@
-<div class="row">
-    <div class="col-md-12">
-        <div class="card">
-            <div class="position-relative">
-                <img class="card-img-top" src="../../../assets/images/backgrounds/fcgr.jpg" alt="Card image cap" style="max-height: 500px">
-                <div class="card-img-overlay p-4">
-                    <div class="text-white mt-3">
-                        <span>
-                            <iconify-icon icon="solar:cloud-sun-bold-duotone" class="display-4"></iconify-icon>
-                        </span>
-                        <div class="mb-2 mt-4">
-                            <span class="display-6" id="fechaActual">
-                            </span>
-                        </div>
-                        <p class="fs-3 mb-0 opacity-80" ></p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="card-footer text-bg-white">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="row text-center">
-                            <!-- Acceso directo a la funcionalidad "Seguimiento" -->
-                            <div class="col-6 col-md border-end">
-                                <div class="mb-2">Seguimiento</div>
-                                <i class="ti ti-clipboard-check fs-9 mb-2"></i>
-                                <div>
-                                    <a href="../lineaBaseAdministracion/" class="btn btn-sm btn-outline-primary">Acceder</a>
-                                </div>
-                            </div>
-                            <!-- Acceso directo a la funcionalidad "Estudio socioeconómico" -->
-                            <div class="col-6 col-md border-end">
-                                <div class="mb-2">Estudio socioeconómico</div>
-                                <i class="ti ti-wallet fs-9 mb-2"></i>
-                                <div>
-                                    <a href="../estudioSocioeconomico/" class="btn btn-sm btn-outline-success">Acceder</a>
-                                </div>
-                            </div>
-                            <!-- Acceso directo a la funcionalidad "Reportes" -->
-                            <div class="col-6 col-md border-end">
-                                <div class="mb-2">Reportes</div>
-                                <i class="ti ti-chart-line fs-9 mb-2"></i>
-                                <div>
-                                    <a href="#verReportes" class="btn btn-sm btn-outline-info">Ver</a>
-                                </div>
-                            </div>
-                            <!-- Acceso directo a la funcionalidad "Configuración" -->
-                            <div class="col-6 col-md border-end">
-                                <div class="mb-2">Configuración</div>
-                                <i class="ti ti-settings fs-9 mb-2"></i>
-                                <div>
-                                    <a href="#configuracion" class="btn btn-sm btn-outline-warning">Configurar</a>
-                                </div>
-                            </div>
+<div class="container py-5">
+    <div class="row g-4">
+        <div class="col-12 col-md-6 col-lg-3">
+            <div class="card h-100 border-0 overflow-hidden">
+                <div class="card-body d-flex flex-column justify-content-between p-4">
+                    <div class="mb-4">
+                        <div class="bg-primary bg-opacity-25 rounded-pill p-3 d-inline-flex">
+                            <i class="ti ti-clipboard-check fs-4 text-primary"></i>
                         </div>
                     </div>
+                    <div>
+                        <h4 class="fw-light mb-2 lh-1 text-dark">Ficha de Seguimiento</h4>
+                    </div>
+                    <a href="../lineaBaseAdministracion/" class="btn btn-outline-primary btn-sm border-2 fw-normal stretched-link">
+                        Acceder <i class="ti ti-arrow-up-right ms-1"></i>
+                    </a>
                 </div>
             </div>
         </div>
-    </div>
-</div>
+
+        <div class="col-12 col-md-6 col-lg-3">
+            <div class="card h-100 border-0 overflow-hidden">
+                <div class="card-body d-flex flex-column justify-content-between p-4">
+                    <div class="mb-4">
+                        <div class="bg-success bg-opacity-25 rounded-pill p-3 d-inline-flex">
+                            <i class="ti ti-wallet fs-4 text-success"></i>
+                        </div>
+                    </div>
+                    <div>
+                        <h4 class="fw-light mb-2 lh-1 text-dark">Estudio Socioeconómico</h4>
+                    </div>
+                    <a href="../estudioSocioeconomico/" class="btn btn-outline-success btn-sm border-2 fw-normal stretched-link">
+                        Acceder <i class="ti ti-arrow-up-right ms-1"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-md-6 col-lg-3">
+            <div class="card h-100 border-0 overflow-hidden">
+                <div class="card-body d-flex flex-column justify-content-between p-4">
+                    <div class="mb-4">
+                        <div class="bg-info bg-opacity-25 rounded-pill p-3 d-inline-flex">
+                            <i class="ti ti-chart-line fs-4 text-info"></i>
+                        </div>
+                    </div>
+                    <div>
+                        <h4 class="fw-light mb-2 lh-1 text-dark">Reportes</h4>
+                    </div>
+                    <a href="#" class="btn btn-outline-info btn-sm border-2 fw-normal stretched-link">
+                        Ver <i class="ti ti-arrow-up-right ms-1"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-md-6 col-lg-3">
+            <div class="card h-100 border-0 overflow-hidden">
+                <div class="card-body d-flex flex-column justify-content-between p-4">
+                    <div class="mb-4">
+                        <div class="bg-warning bg-opacity-25 rounded-pill p-3 d-inline-flex">
+                            <i class="ti ti-settings fs-4 text-warning"></i>
+                        </div>
+                    </div>
+                    <div>
+                        <h4 class="fw-light mb-2 lh-1 text-dark">Configuración</h4>
+                    </div>
+                    <a href="#" class="btn btn-outline-warning btn-sm border-2 fw-normal stretched-link">
+                        Configurar <i class="ti ti-arrow-up-right ms-1"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div> <!-- Fin de .row -->
+</div> <!-- Fin de .container -->
