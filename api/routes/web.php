13be7ed105b733590262562a36b0d335d7161148
Juan Carlos Gonzalez <juan.gonzalez@alumnos.udg.mx>
2025-09-08 16:53:03 -0600

    - Ruta test eliminada - Integración de los access points como ruta absoluta

--- api/routes/web.php
+++ api/routes/web.php
@@ -17,31 +17,25 @@
 
 $router->get(
     '/', function () use ($router) {
+        $appUrl = env('APP_URL');
         $result = [
             'app'     => 'Fundacion Garibi Rivera API',
             'framework-version' => $router->app->version(),
             'app-version' => "2025.08.24",
             'author'  => 'Juan Carlos Gonzalez A.',
             'accesPoints' => [
-                'POST /auth/verify-email' => 'Verifica si un correo electrónico ya está registrado y envía un código de verificación si no lo está.',
-                'POST /auth/verify-code' => 'Verifica el código de verificación enviado al correo electrónico del usuario.',
-                'POST /auth/register' => 'Registra una nueva cuenta de usuario después de verificar el código.',
-                'POST /auth/login' => 'Autentica a un usuario y devuelve un token de acceso.',
-                'POST /auth/logout' => 'Cierra la sesión del usuario.',
-                'POST /auth/refresh' => 'Renueva el token de acceso del usuario.',
-                'POST /auth/me' => 'Obtiene la información del usuario autenticado.',
+                'POST ' . $appUrl . '/auth/verify-email' => 'Verifica si un correo electrónico ya está registrado y envía un código de verificación si no lo está.',
+                'POST ' . $appUrl . '/auth/verify-code' => 'Verifica el código de verificación enviado al correo electrónico del usuario.',
+                'POST ' . $appUrl . '/auth/register' => 'Registra una nueva cuenta de usuario después de verificar el código.',
+                'POST ' . $appUrl . '/auth/login' => 'Autentica a un usuario y devuelve un token de acceso.',
+                'POST ' . $appUrl . '/auth/logout' => 'Cierra la sesión del usuario.',
+                'POST ' . $appUrl . '/auth/refresh' => 'Renueva el token de acceso del usuario.',
+                'GET ' . $appUrl . '/auth/me' => 'Obtiene la información del usuario autenticado.'
             ]
         ];
         return response()->json($result);
     }
 );
-
-$router->get(
-    '/test', function () {
-        return 'Hello World';
-    }
-);
-
 
 $router->group(
     ['prefix' => 'auth'], function () use ($router) {
