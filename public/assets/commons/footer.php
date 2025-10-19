34e4a96d85b4e6c4a8b08cdc46267198987454c8
Jan Karol Dev <juan.gonzalez@alumnos.udg.mx>
2025-10-19 17:37:38 -0600

    Se remueven los enlaces de video institucional e Informe anual

--- public/assets/commons/footer.php
+++ public/assets/commons/footer.php
@@ -1,25 +1,5 @@
 <footer class="footer bg-light text-dark mt-5">
     <div class="container">
-        <div class="d-flex justify-content-between align-items-center flex-column flex-sm-row py-4">
-            <!-- Sección de navegación -->
-            <nav class="mb-3 mb-sm-0">
-                <ul class="nav">
-                    <li class="nav-item">
-                        <a class="nav-link text-success" href="https://www.youtube.com/watch?v=XhIXDH55mrk">
-                            <span><i class="ti ti-brand-youtube text-danger"></i></span>
-                            Video Institucional
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-success" href="https://www.fundaciongaribirivera.com/publicaciones/8-2/">
-                            <span><i class="ti ti-file-report text-primary"></i></span>
-                            Informe Anual
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-
         <!-- Copyright Section -->
         <div class="copyright text-center text-success py-3">
             <p class="mb-0">
