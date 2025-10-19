376e13e32a8b4c8a3db972e794d9323e7a994494
Jan Karol Dev <juan.gonzalez@alumnos.udg.mx>
2025-10-19 17:37:58 -0600

    Indentación mejorada

--- public/assets/commons/template.php
+++ public/assets/commons/template.php
@@ -1,75 +1,78 @@
-
 <?php
 
-function inicializarPlantilla($dir, $aside, $scripts = []) {
+function inicializarPlantilla($dir, $aside, $scripts = [])
+{
     ?>
     <!DOCTYPE html>
     <html lang="es" dir="ltr" data-bs-theme="light" data-color-theme="Blue_Theme">
-        <head>
+
+    <head>
+        <?php
+        // Incluir archivo de la cabecera
+        include '../../../assets/commons/head.php';
+        ?>
+    </head>
+
+    <body>
+        <!-- Preloader -->
+        <?php
+        include '../../../assets/commons/preloader.php';
+        ?>
+
+        <div id="main-wrapper">
+            <!-- Sidebar Start -->
             <?php
-            // Incluir archivo de la cabecera
-            include '../../../assets/commons/head.php';
+            include $aside;
             ?>
-        </head>
-        <body>
-            <!-- Preloader -->
-            <?php
-            include '../../../assets/commons/preloader.php';
-            ?>
+            <!-- Sidebar End -->
 
-            <div id="main-wrapper">
-                <!-- Sidebar Start -->
-                <?php
-                include $aside;
-                ?>
-                <!-- Sidebar End -->
+            <div class="page-wrapper">
+                <div class="body-wrapper">
+                    <div class="container-fluid">
+                        <header class="topbar sticky-top">
+                            <div class="with-vertical">
+                                <!-- Start Vertical Layout Header -->
+                                <?php
+                                include '../../../assets/commons/header.php';
+                                ?>
+                                <!-- End Vertical Layout Header -->
+                                <!-- Mobile Navbar -->
+                                <?php
+                                // Si deseas habilitar la barra de navegación móvil, puedes descomentar esta línea
+                                // include '../../../assets/commons/mobilenavbar.php';
+                                ?>
+                            </div>
+                        </header>
 
-                <div class="page-wrapper">
-                    <div class="body-wrapper">
-                        <div class="container-fluid">
-                            <header class="topbar sticky-top">
-                                <div class="with-vertical">
-                                    <!-- Start Vertical Layout Header -->
-                                    <?php
-                                    include '../../../assets/commons/header.php';
-                                    ?>
-                                    <!-- End Vertical Layout Header -->
-                                    <!-- Mobile Navbar -->
-                                    <?php
-                                    // Si deseas habilitar la barra de navegación móvil, puedes descomentar esta línea
-                                    // include '../../../assets/commons/mobilenavbar.php';
-                                    ?>
+                        <main>
+                            <div class="row">
+                                <div class="align-items-stretch">
+                                    <!-- Incluir contenido específico de la página -->
+                                    <?php include_once $dir . "/content.php"; ?>
                                 </div>
-                            </header>
+                            </div>
+                        </main>
 
-                            <main>
-                                <div class="row">
-                                    <div class="align-items-stretch">
-                                        <!-- Incluir contenido específico de la página -->
-    <?php include_once $dir . "/content.php"; ?>
-                                    </div>
-                                </div>
-                            </main>
-
-                            <!-- Incluir footer -->
-    <?php include '../../../assets/commons/footer.php'; ?>
-                        </div>
+                        <!-- Incluir footer -->
+                        <?php include '../../../assets/commons/footer.php'; ?>
                     </div>
                 </div>
             </div>
+        </div>
 
-            <!-- Incluir scripts generales -->
-            <?php
-            include '../../../assets/commons/scripts.php';
-            ?>
+        <!-- Incluir scripts generales -->
+        <?php
+        include '../../../assets/commons/scripts.php';
+        ?>
 
-            <!-- Incluir scripts adicionales, si los hay -->
-            <?php
-            foreach ($scripts as $script) {
-                echo '<script src="' . $script . '"></script>';
-            }
-            ?>
-        </body>
+        <!-- Incluir scripts adicionales, si los hay -->
+        <?php
+        foreach ($scripts as $script) {
+            echo '<script src="' . $script . '"></script>';
+        }
+        ?>
+    </body>
+
     </html>
     <?php
 }
