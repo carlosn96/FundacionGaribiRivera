ae969b8af7d1556913915c99ac744d777dd947cf
Jan Karol Dev <juan.gonzalez@alumnos.udg.mx>
2025-10-25 15:04:24 -0600

    feat(SeguimimientoGraduado): CRUD para el seguimiento de Graduado

---
+++ public/emprendimiento/modules/seguimientoGraduado/api/SeguimientoGraduadoAPI.php
@@ -0,0 +1,43 @@
+<?php
+
+include_once '../../../../../loader.php';
+
+class SeguimientoGraduadoAPI extends API
+{
+    public function recuperarCamposInformacion()
+    {
+        $adminLineaBase = getAdminLineaBase();
+        $emprendedor = getAdminEmprendedor()->get($id = Sesion::getInfoTemporal("emprendedor"));
+        $lineaBase = $adminLineaBase->getLineaBase(Sesion::getInfoTemporal("usuarioEmprendedor"));
+        $this->enviarRespuesta([
+            "emprendedor" => $emprendedor,
+            "seguimientoGraduado" => $this->getSeguimiento($id),
+            "lineaBase" => $lineaBase["inicial"],
+            "checkbox" => [
+                "estrategiasIncrementarVentas" => $adminLineaBase->recuperarListaEstrategiasIncrementarVentas()
+            ],
+            "radio" => [
+                "comoEmpleaGanancias" => $adminLineaBase->recuperarListaEmpleaGanancias()
+            ]
+        ]);
+    }
+
+    public function guardar()
+    {
+        $this->enviarResultadoOperacion(
+            getAdminImpacto()->guardarSeguimientoGraduado($this->data)
+        );
+    }
+
+    private function getSeguimiento($id)
+    {
+        $adminImpacto = getAdminImpacto();
+        return [
+            "existeSeguimientoGraduado" => $adminImpacto->existeSeguimientoGraduado($id),
+            "data" => $adminImpacto->recuperarSeguimientoGraduado($id)
+        ];
+    }
+
+}
+
+SeguimientoGraduadoAPI::start();
